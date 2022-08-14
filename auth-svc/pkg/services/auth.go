aa2035308f9170d7ed45b9789ff41fdf1676733d
Satriyo Aji <satriyoaji27@gmail.com>
2022-08-14 16:20:34 +0700

    done authentication microservice

---
+++ auth-svc/pkg/services/auth.go
@@ -0,0 +1,88 @@
+package services
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/satriyoaji/auth-svc/pkg/db"
+	"github.com/satriyoaji/auth-svc/pkg/models"
+	"github.com/satriyoaji/auth-svc/pkg/pb"
+	"github.com/satriyoaji/auth-svc/pkg/utils"
+)
+
+type Server struct {
+	H   db.Handler
+	Jwt utils.JwtWrapper
+}
+
+func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	var user models.User
+
+	if result := s.H.DB.Where(&models.User{Email: req.Email}).First(&user); result.Error == nil {
+		return &pb.RegisterResponse{
+			Status: http.StatusConflict,
+			Error:  "E-Mail already exists",
+		}, nil
+	}
+
+	user.Email = req.Email
+	user.Password = utils.HashPassword(req.Password)
+
+	s.H.DB.Create(&user)
+
+	return &pb.RegisterResponse{
+		Status: http.StatusCreated,
+	}, nil
+}
+
+func (s *Server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	var user models.User
+
+	if result := s.H.DB.Where(&models.User{Email: req.Email}).First(&user); result.Error != nil {
+		return &pb.LoginResponse{
+			Status: http.StatusNotFound,
+			Error:  "Your credential doesn't match our records",
+		}, nil
+	}
+
+	match := utils.CheckPasswordHash(req.Password, user.Password)
+
+	if !match {
+		return &pb.LoginResponse{
+			Status: http.StatusNotFound,
+			Error:  "Your credential doesn't match our records",
+		}, nil
+	}
+
+	token, _ := s.Jwt.GenerateToken(user)
+
+	return &pb.LoginResponse{
+		Status: http.StatusOK,
+		Token:  token,
+	}, nil
+}
+
+func (s *Server) Validate(ctx context.Context, req *pb.ValidateRequest) (*pb.ValidateResponse, error) {
+	claims, err := s.Jwt.ValidateToken(req.Token)
+
+	if err != nil {
+		return &pb.ValidateResponse{
+			Status: http.StatusBadRequest,
+			Error:  err.Error(),
+		}, nil
+	}
+
+	var user models.User
+
+	if result := s.H.DB.Where(&models.User{Email: claims.Email}).First(&user); result.Error != nil {
+		return &pb.ValidateResponse{
+			Status: http.StatusNotFound,
+			Error:  "Your credential doesn't match our records",
+		}, nil
+	}
+
+	return &pb.ValidateResponse{
+		Status: http.StatusOK,
+		UserId: user.Id,
+	}, nil
+}
